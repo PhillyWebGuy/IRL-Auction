900ee5071a8d2f5b1c68941988ce6643b8d286d4
Ed Hertzog <ehertzog@intuitivecompany.com>
2014-03-07 22:08:47 -0500

    laying down the jawn

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global IRLAuction, $*/
+
+
+window.IRLAuction = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    IRLAuction.init();
+});
